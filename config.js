c281a98c5ef189222eeae46062e2fff2ad32175b
Gary Anderson <ganderson@seattleacademy.org>
2017-03-08 09:03:16 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+        plugins: [
+        //    "video-png"     // Display the video feed as static pngs (work in every browser)
+        , "video-stream"  // Display the video as a native h264 stream decoded in JS 
+          , "hud"           // Display the artificial horizon, altimeter, compass, etc.
+          , "battery"       // Display a simple battery widget in the header bar
+          , "pilot"         // Pilot the drone with the keyboard
+        ],
+
+        // Config for pilot plugin
+        keyboard: 'qwerty',
+
+};
+
+module.exports = config;
+
