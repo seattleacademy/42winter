a8d02663f44229834de9ae1be0a166760ce7d5e8
SAAS <SAAS@ariporad.local>
2017-03-10 11:48:26 -0800

    feat: setup tracking
    This can now track the red and green cups, and will flip when the are crossed

--- plugins/track/index.js
+++ plugins/track/index.js
@@ -1,7 +1,15 @@
 function track(name, deps){
-	//console.log("tracker activated")
-
-
+	console.log("tracker activated")
+	deps.io.sockets.on('connection', function (socket) {
+        socket.on('/track/flipLeft', function () {
+	   console.log('flipLeft');
+           return deps.client.animate('flipLeft', 500);
+        });
+        socket.on('/track/flipRight', function () {
+	   console.log('flipRight');
+           return deps.client.animate('flipRight', 500);
+        });
+	});
 };
 
 module.exports = track
